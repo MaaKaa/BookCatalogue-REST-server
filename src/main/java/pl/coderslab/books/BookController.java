216af31037fa5915b70f716c3986b093eaa20971
Marzena <kacprowicz.marzena@gmail.com>
2019-03-30 20:17:44 +0100

    MemoryBookService updated - methods: getAllBooks, getBookById, updateBook and deleteBook added. Access points - actions:  getAllBooks, getBookById, addBook, updateBook and deleteBook added to the BookController.

--- src/main/java/pl/coderslab/books/BookController.java
+++ src/main/java/pl/coderslab/books/BookController.java
@@ -1,8 +1,8 @@
 package pl.coderslab.books;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import pl.coderslab.books.Book;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
@@ -17,4 +17,45 @@ public class BookController {
         return new Book(1L,"9788324631766","Thinking in Java",
                 "Bruce Eckel","Helion","programming");
     }
+
+    //Utwórz zmienną tej klasy w kontrolerze, a następnie wstrzyknij ją:
+    private MemoryBookService memoryBookService;
+
+    //Dodaj akcję kontrolera, która zwróci listę wszystkich książek:
+    @GetMapping("/")
+    @ResponseBody
+    public List<Book> getAllBooks(){
+        return new MemoryBookService().getList();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Book getBookById(@PathVariable("id") long id){
+        return new MemoryBookService().getBookById(id);
+    }
+
+    @PostMapping("/")
+    public void addBook(@RequestParam(name="isbn") String isbn,
+                        @RequestParam(name="title") String title,
+                        @RequestParam(name="author") String author,
+                        @RequestParam(name="publisher") String publisher,
+                        @RequestParam(name="type") String type){
+        new MemoryBookService().addBook(isbn, title, author, publisher, type);
+    }
+
+    @PutMapping("/{id}")
+    public void updateBook(@PathVariable(name="id") String id,
+                           @RequestParam(name="isbn") String isbn,
+                           @RequestParam(name="title") String title,
+                           @RequestParam(name="author") String author,
+                           @RequestParam(name="publisher") String publisher,
+                           @RequestParam(name="type") String type){
+        new MemoryBookService().addBook(isbn, title, author, publisher, type);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable(name="id") long id){
+        new MemoryBookService().deleteBook(id);
+    }
+
 }
--- src/main/java/pl/coderslab/books/MemoryBookService.java
+++ src/main/java/pl/coderslab/books/MemoryBookService.java
@@ -1,13 +1,17 @@
 package pl.coderslab.books;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MemoryBookService {
+    //Creation and initialization of a book list:
     private List<Book> list;
+
     public MemoryBookService() {
         list = new ArrayList<>();
         list.add(new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel",
@@ -17,6 +21,64 @@ public class MemoryBookService {
         list.add(new Book(3L, "9780130819338", "Java 2. Podstawy",
                 "Cay Horstmann, Gary Cornell", "Helion", "programming"));
     }
-    public List<Book> getList() {return list;}
-    public void setList(List<Book> list) {this.list = list;}
+
+    public List<Book> getList() {
+        return list;}
+
+    public void setList(List<Book> list) {
+        this.list = list;
+    }
+
+    @GetMapping(path = "/",  produces = "application/json")
+    public List<Book> getAllBooks() {
+        return getList();
+    }
+
+    @GetMapping(path = "{id}", produces = "application/json")
+    public Book getBookById(@PathVariable(name = "id") long id) {
+        Book book = null;
+        for (Book bookToFind : list){
+            if(bookToFind.getId() == id){
+                book = bookToFind;
+            }else{
+                System.out.println("Cannot find book with id " + id);
+            }
+        }
+        return book;
+    }
+
+    @PostMapping(path = "/", produces = "application/json")
+    public void addBook(String isbn, String title, String author, String publisher, String type){
+        Book newBook = new Book(isbn, title, author, publisher, type);
+        list.add(newBook);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updateBook(@PathVariable(name="id") long id, String isbn, String title, String author, String publisher, String type){
+        for (Book bookToFind : list){
+            if(bookToFind.getId() == id){
+                bookToFind.setIsbn(isbn);
+                bookToFind.setTitle(title);
+                bookToFind.setAuthor(author);
+                bookToFind.setPublisher(publisher);
+                bookToFind.setType(type);
+                System.out.println("Updated book with id " + id);
+            }else{
+                System.out.println("Cannot find book with id " + id);
+            }
+        }
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteBook(@PathVariable(name="id") long id){
+        for (Book bookToFind : list){
+            if(bookToFind.getId() == id){
+                list.remove(bookToFind);
+                System.out.println("Removed book with id " + id);
+            }else{
+                System.out.println("Cannot find book with id " + id);
+            }
+        }
+    }
+
 }
